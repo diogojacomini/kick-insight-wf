03591fa0ae7e5a326dbc6d8501c5e7f2027dc094
Diogo Leme Jacomini <diogojacomini@brq.com>
2023-11-18 20:26:19 -0300

    clone all

--- git_clone.py
+++ git_clone.py
@@ -6,7 +6,6 @@ github_repo_url = "https://github.com/diogojacomini/kick-insight-wf"
 local_repo_path = "/tmp/kick-insight-wf"
 
 os.system(f"git clone {github_repo_url} {local_repo_path}")
-os.system(f"git checkout develop")
 
 for root, dirs, files in os.walk(local_repo_path):
     for file in files:
